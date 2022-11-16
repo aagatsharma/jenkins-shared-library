5219011c14592c3d5041d85844eab09196786744
Aagat <aagat@localhost.localdomain>
2022-11-16 16:13:54 +0545

    Hope works now

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,4 +6,5 @@ def call(){
         sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
+    }
 }
